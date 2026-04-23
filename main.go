c7230907759a0537803d2fc44f8b644cbcd75125
chris <yeditor@gmail.com>
2026-04-23 11:09:55 +0800

    調整共用資訊

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"time"
 
+	"idv/chris/api"
 	"idv/chris/model"
 	"idv/chris/utils"
 )
 
+func toJSON(source any) string {
+	content, err := json.Marshal(source)
+	if err != nil {
+		panic(err)
+	}
+	return string(content)
+}
+
 func main() {
 	setting := model.NewSetting()
-	fmt.Println(setting)
 
-	// now := time.Now().Unix()
+	tmp := setting.Prepare.Content
+	tmp["Timestamp"] = time.Now().Unix()
+	tmp["Sign"] = utils.GenSign(tmp, setting.APIKey)
 
-	fmt.Println(utils.GenSign(setting.Prepare.Content, setting.APIKey))
+	api.ReqAuth(setting.Host, toJSON(tmp))
 }
