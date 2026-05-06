36861dae39f0674c8bccd059c0f13406ccef8ab3
chris <yeditor@gmail.com>
2026-05-06 17:50:25 +0800

    加入檔案功能

--- api/Task.go
+++ api/Task.go
@@ -3,10 +3,36 @@ package api
 import (
 	"encoding/json"
 	"fmt"
+	"idv/chris/utils"
 	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
+var (
+	dir_path string = ""
+)
+
+func init() {
+	dir_path = time.Now().Format("20060102")
+}
+
 func ReqTask(host, action, token, body string) any {
+	log_msg := `
+===== ACTION =====
+%s
+===== BODY =====
+%s
+===== =====
+
+`
+
+	defer func() {
+		file_name := strings.ReplaceAll(action, "/", "_")
+		utils.FileWriteAppend(filepath.Join("./", dir_path), file_name+".txt", []byte(fmt.Sprintf(log_msg, action, body)))
+	}()
+
 	fmt.Printf("\n\n===== Action(%s) =====\n\n", action)
 	cmd_statement := exec.Command(
 		"curl", "-s",
@@ -26,6 +52,7 @@ func ReqTask(host, action, token, body string) any {
 	// err = json.Indent(&pretty_json, output, "", "  ")
 
 	fmt.Printf("\nBeforeJSON:\n%s\n", string(output))
+	log_msg += string(output)
 
 	var data any
 	err = json.Unmarshal(output, &data)
